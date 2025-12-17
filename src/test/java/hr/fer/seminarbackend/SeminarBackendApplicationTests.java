7b21bb9b688522dee8564d6457d1142f6665ac3f
Renato Malbašić <=>
2025-12-17 22:59:08 +0100

    setup base project

---
+++ src/test/java/hr/fer/seminarbackend/SeminarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.seminarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
